01788bfb84d52defb8d6b5c5ad5e44fc674c1256
Kosuke1212 <n.kosuke0804@gmail.com>
2025-04-18 15:58:51 +0900

    set neotree hl

--- lua/colors/solarized-osaka.lua
+++ lua/colors/solarized-osaka.lua
@@ -6,14 +6,19 @@ return {
 		require("solarized-osaka").setup({
 			transparent = true,
 			italic = true,
-
 			-- Neotree transparent settings
-			on_highlights = function(hl)
+			on_highlights = function(hl, c)
 				hl.NeoTreeNormal = {
 					bg = "none",
 				}
 				hl.NeoTreeNormalNC = {
 					bg = "none",
+				}
+				hl.NeoTreeDirectoryIcon = {
+					fg = c.cyan,
+				}
+				hl.NeoTreeDirectoryName = {
+					fg = c.cyan,
 				}
 			end,
 		})
