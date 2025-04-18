54bfc37e793be7a1df0b8b4ba68ebe9ce5e3c0fd
Kosuke1212 <n.kosuke0804@gmail.com>
2025-04-18 15:59:38 +0900

    change git sings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,12 +2,13 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPost", "BufNewFile" },
 	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "_" },
-		},
+		signcolumn = true,
+		-- signs = {
+		-- 	add = { text = "+" },
+		-- 	change = { text = "~" },
+		-- 	delete = { text = "_" },
+		-- 	topdelete = { text = "‾" },
+		-- 	changedelete = { text = "_" },
+		-- },
 	},
 }
