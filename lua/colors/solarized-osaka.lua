d16bbdd2562ca2226b67099b87bd7482c0d53271
K.N <n.kosuke0804@gmail.com>
2025-07-02 16:56:21 +0900

    added colorschemes

---
+++ lua/colors/solarized-osaka.lua
@@ -0,0 +1,27 @@
+return {
+	"craftzdog/solarized-osaka.nvim",
+	-- lazy = true,
+	priority = 1000,
+	config = function()
+		require("solarized-osaka").setup({
+			transparent = true,
+			italic = true,
+			-- Neotree transparent settings
+			on_highlights = function(hl, c)
+				hl.NeoTreeNormal = {
+					bg = "none",
+				}
+				hl.NeoTreeNormalNC = {
+					bg = "none",
+				}
+				hl.NeoTreeDirectoryIcon = {
+					fg = c.cyan,
+				}
+				hl.NeoTreeDirectoryName = {
+					fg = c.cyan,
+				}
+			end,
+		})
+		-- vim.cmd("colorscheme solarized-osaka")
+	end,
+}
