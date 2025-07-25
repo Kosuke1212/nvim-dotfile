5185055ceae54f52697fa623066efa9eb944ae38
K.N <n.kosuke0804@gmail.com>
2025-07-25 17:00:46 +0900

    lsp diagnostics config

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -4,3 +4,23 @@ local lsp_names = {
 }
 
 vim.lsp.enable(lsp_names)
+
+-- diagnostics signs
+--
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
+        },
+        linehl = {
+            -- [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+        numhl = {
+            -- [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            -- [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
+})
